6f996d320e13b9efea0129e3752a0e3cbe4f66a8
danielskjo <sungkeun.jo@gmail.com>
2022-01-21 20:15:18 -0800

    Set up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyAv2hQAY4XPPj3xEvyDKLQaC9b6Tb09JQw",
+        appId: "1:516653149906:web:906d62cef9a0b5b9f1070b",
+        messagingSenderId: "516653149906",
+        projectId: "instagram-c505a",
+        storageBucket: "instagram-c505a.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
